ae87984a6fb3524b2327c0b3d6bf02c4e1f7b3af
Rommel Canoy <rommelcanoy4@gmail.com>
2023-08-08 01:16:46 +0800

    feat: add languageContentUpdater.js

---
+++ languageContentUpdater.js
@@ -0,0 +1,362 @@
+var langs = {
+en: {
+ step1: "Preparing the download ...",
+ step2: "If your download doesn't start automatically ...",
+ step3: "Step 1: Click OK to go to the download page",
+ step4: "Step 2: Verify the CAPTCHA to start the download",
+ }
+ ,
+cs: {
+ step1: "Příprava stahování ...",
+ step2: "Pokud se stahování nespustí automaticky ...",
+ step3: "Krok 1: Kliknutím na OK přejděte na stránku stahování",
+ step4: "Krok 2: Chcete-li zahájit stahování, ověřte CAPTCHA",
+ }
+ ,
+et: {
+ step1: "Allalaadimise ettevalmistamine ...",
+ step2: "Kui teie allalaadimine ei käivitu automaatselt ...",
+ step3: "1. samm: klõpsake allalaadimislehele minemiseks nuppu OK",
+ step4: "2. samm: allalaadimise alustamiseks kontrollige CAPTCHA-d",
+ }
+ ,
+de: {
+ step1: "Download vorbereiten ...",
+ step2: "Wenn Ihr Download nicht automatisch startet ...",
+ step3: "Schritt 1: Klicken Sie auf OK, um zur Download-Seite zu gelangen",
+ step4: "Schritt 2: Überprüfen Sie das CAPTCHA, um den Download zu starten",
+ }
+ ,
+it: {
+ step1: "Preparazione del download ...",
+ step2: "Se il download non si avvia automaticamente ...",
+ step3: "Passaggio 1: fare clic su OK per accedere alla pagina di download",
+ step4: "Passaggio 2: verifica il CAPTCHA per avviare il download",
+ }
+ ,  
+lt: {
+ step1: "Ruošiamasi atsisiųsti ...",
+ step2: "Jei atsisiuntimas neprasideda automatiškai ...",
+ step3: "1 žingsnis: Spustelėkite Gerai, kad pereitumėte į atsisiuntimo puslapį",
+ step4: "2 veiksmas: patikrinkite CAPTCHA, kad pradėtumėte atsisiuntimą",
+ }
+ ,
+no: {
+ step1: "Forbereder nedlastingen ...",
+ step2: "Hvis nedlastingen ikke starter automatisk ...",
+ step3: "Trinn 1: Klikk OK for å gå til nedlastingssiden",
+ step4: "Trinn 2: Bekreft CAPTCHA for å starte nedlastingen",
+ }
+ ,
+ru: {
+ step1: "Подготовка к загрузке ...",
+ step2: "Если загрузка не начинается автоматически ...",
+ step3: "Шаг 1. Нажмите ОК, чтобы перейти на страницу загрузки.",
+ step4: "Шаг 2. Проверьте CAPTCHA, чтобы начать загрузку.",
+ }
+ , 
+es: {
+ step1: "Preparando la descarga ...",
+ step2: "Si su descarga no se inicia automáticamente ...",
+ step3: "Paso 1: haga clic en Aceptar para ir a la página de descarga",
+ step4: "Paso 2: Verifique el CAPTCHA para iniciar la descarga",
+ }
+ ,  
+da: {
+ step1: "Forbereder download ...",
+ step2: "Hvis din download ikke starter automatisk ...",
+ step3: "Trin 1: Klik på OK for at gå til download-siden",
+ step4: "Trin 2: Bekræft CAPTCHA for at starte overførslen",
+ }
+ , 
+fil: {
+ step1: "Inihahanda ang pag-download ...",
+ step2: "Kung ang iyong pag-download ay hindi awtomatikong nagsisimula ...",
+ step3: "Hakbang 1: Mag-click sa OK upang pumunta sa pahina ng pag-download",
+ step4: "Hakbang 2: I-verify ang CAPTCHA upang simulan ang pag-download",
+ }
+ , 
+el: {
+ step1: "Προετοιμασία της λήψης ...",
+ step2: "Εάν η λήψη δεν ξεκινά αυτόματα ...",
+ step3: "Βήμα 1: Κάντε κλικ στο OK για να μεταβείτε στη σελίδα λήψης",
+ step4: "Βήμα 2: Επαληθεύστε το CAPTCHA για να ξεκινήσετε τη λήψη",
+ }
+ ,   
+ja: {
+ step1: "ダウンロードの準備..。",
+ step2: "ダウンロードが自動的に開始されない場合...",
+ step3: "ステップ1：[OK]をクリックしてダウンロードページに移動します",
+ step4: "ステップ2：CAPTCHAを確認してダウンロードを開始します",
+ }
+ ,  
+ms: {
+ step1: "Menyiapkan muat turun ...",
+ step2: "Sekiranya muat turun anda tidak bermula secara automatik ...",
+ step3: "Langkah 1: Klik OK untuk pergi ke halaman muat turun",
+ step4: "Langkah 2: Sahkan CAPTCHA untuk memulakan muat turun",
+ }
+ , 
+ pl: {
+ step1: "Przygotowanie do pobrania ...",
+ step2: "Jeśli pobieranie nie rozpocznie się automatycznie ...",
+ step3: "Krok 1: Kliknij OK, aby przejść do strony pobierania",
+ step4: "Krok 2: Sprawdź CAPTCHA, aby rozpocząć pobieranie",
+ }
+ , 
+ sr: {
+ step1: "Припрема се преузимање ...",
+ step2: "Ако се преузимање не започне аутоматски ...",
+ step3: "Корак 1: Кликните на дугме У реду да бисте прешли на страницу за преузимање",
+ step4: "Корак 2: Потврдите ЦАПТЦХА да бисте започели преузимање",
+ }
+ ,  
+ sv: {
+ step1: "Förbereder nedladdningen ...",
+ step2: "Om din nedladdning inte startar automatiskt ...",
+ step3: "Steg 1: Klicka på OK för att gå till nedladdningssidan",
+ step4: "Steg 2: Verifiera CAPTCHA för att starta nedladdningen",
+ }
+ ,   
+ th: {
+ step1: "กำลังเตรียมการดาวน์โหลด ...",
+ step2: "หากการดาวน์โหลดของคุณไม่เริ่มโดยอัตโนมัติ ...",
+ step3: "ขั้นตอนที่ 1: คลิกตกลงเพื่อไปที่หน้าดาวน์โหลด",
+ step4: "ขั้นตอนที่ 2: ตรวจสอบ CAPTCHA เพื่อเริ่มการดาวน์โหลด",
+ }
+ , 
+ nl: {
+ step1: "De download voorbereiden ...",
+ step2: "Als uw download niet automatisch start ...",
+ step3: "Stap 1: Klik op OK om naar de downloadpagina te gaan",
+ step4: "Stap 2: Verifieer de CAPTCHA om de download te starten",
+ }
+ , 
+ nl: {
+ step1: "Valmistellaan latausta ...",
+ step2: "Jos lataus ei ala automaattisesti ...",
+ step3: "Vaihe 1: Napsauta OK siirtyäksesi lataussivulle",
+ step4: "Vaihe 2: Vahvista CAPTCHA aloittaaksesi latauksen",
+ }
+ , 
+ ko: {
+ step1: "다운로드 준비 중 ...",
+ step2: "J다운로드가 자동으로 시작되지 않는 경우 ...",
+ step3: "1 단계 : 확인을 클릭하여 다운로드 페이지로 이동",
+ step4: "2 단계 : 보안 문자를 확인하여 다운로드를 시작합니다.",
+ }
+ , 
+ pt: {
+ step1: "Preparando o download ...",
+ step2: "Se o seu download não começar automaticamente ...",
+ step3: "Etapa 1: Clique em OK para ir para a página de download",
+ step4: "Etapa 2: verifique o CAPTCHA para iniciar o download",
+ }
+ , 
+ sk: {
+ step1: "Príprava sťahovania ...",
+ step2: "Ak sa sťahovanie nespustí automaticky ...",
+ step3: "Krok 1: Kliknutím na tlačidlo OK prejdete na stránku sťahovania",
+ step4: "Krok 2: Spustením sťahovania overte obrázok CAPTCHA",
+ }
+ ,
+ tr: {
+ step1: "İndirmeye hazırlanıyor ...",
+ step2: "İndirme işleminiz otomatik olarak başlamazsa ...",
+ step3: "Adım 1: İndirme sayfasına gitmek için Tamam'a tıklayın",
+ step4: "2. Adım: İndirmeyi başlatmak için CAPTCHA'yı doğrulayın",
+ }
+ , 
+ vi: {
+ step1: "Đang chuẩn bị tải xuống ...",
+ step2: "Nếu quá trình tải xuống của bạn không tự động bắt đầu ...",
+ step3: "Bước 1: Nhấp OK để chuyển đến trang tải xuống",
+ step4: "Bước 2: Xác minh CAPTCHA để bắt đầu tải xuống",
+ }
+ ,
+ bg: {
+ step1: "Подготовка за изтегляне ...",
+ step2: "Ако изтеглянето ви не започне автоматично ...",
+ step3: "Стъпка 1: Щракнете върху OK, за да отидете на страницата за изтегляне",
+ step4: "Стъпка 2: Проверете CAPTCHA, за да започнете изтеглянето",
+ }
+ ,
+ hr: {
+ step1: "Priprema za preuzimanje ...",
+ step2: "Ako se preuzimanje ne započne automatski ...",
+ step3: "Korak 1: Kliknite U redu da biste otvorili stranicu za preuzimanje",
+ step4: "Korak 2: Potvrdite CAPTCHA da biste započeli preuzimanje",
+ }
+ ,  
+ fr: {
+ step1: "Préparation du téléchargement ...",
+ step2: "Si votre téléchargement ne démarre pas automatiquement ...",
+ step3: "Étape 1: Cliquez sur OK pour accéder à la page de téléchargement",
+ step4: "Étape 2: Vérifiez le CAPTCHA pour démarrer le téléchargement",
+ }
+ ,  
+ id: {
+ step1: "Mempersiapkan unduhan ...",
+ step2: "Jika unduhan Anda tidak dimulai secara otomatis ...",
+ step3: "Langkah 1: Klik OK untuk membuka halaman unduhan",
+ step4: "Langkah 2: Verifikasi CAPTCHA untuk mulai mengunduh",
+ }
+ ,
+ lv: {
+ step1: "Notiek lejupielādes sagatavošana ...",
+ step2: "Ja lejupielāde nesākas automātiski ...",
+ step3: "1. solis: Noklikšķiniet uz Labi, lai pārietu uz lejupielādes lapu",
+ step4: "2. darbība. Pārbaudiet CAPTCHA, lai sāktu lejupielādi",
+ }
+ , 
+ ro: {
+ step1: "Se pregătește descărcarea ...",
+ step2: "Dacă descărcarea dvs. nu pornește automat ...",
+ step3: "Pasul 1: Faceți clic pe OK pentru a accesa pagina de descărcare",
+ step4: "Pasul 2: Verificați CAPTCHA pentru a începe descărcarea",
+ }
+ , 
+ sl: {
+ step1: "Priprava prenosa ...",
+ step2: "Če se prenos ne začne samodejno ...",
+ step3: "1. korak: Kliknite V redu, da odprete stran za prenos",
+ step4: "2. korak: Za začetek prenosa preverite CAPTCHA",
+ }
+ ,
+ uk: {
+ step1: "Підготовка завантаження ...",
+ step2: "Якщо завантаження не розпочнеться автоматично ...",
+ step3: "Крок 1: Клацніть OK, щоб перейти на сторінку завантаження",
+ step4: "Крок 2: Перевірте CAPTCHA, щоб розпочати завантаженн",
+ }
+ ,    
+ az: {
+ step1: "Endirmə hazırlanır ...",
+ step2: "Yükləmə avtomatik başlamazsa ...",
+ step3: "Addım 1: Yükləmə səhifəsinə getmək üçün Tamam düyməsini basın",
+ step4: "Addım 2: Yükləməyə başlamaq üçün CAPTCHA-nı doğrulayın",
+ }
+ , 
+ hu: {
+ step1: "A letöltés előkészítése ...",
+ step2: "Ha a letöltés nem indul el automatikusan ...",
+ step3: "1. lépés: Kattintson az OK gombra a letöltési oldalra lépéshez",
+ step4: "2. lépés: Ellenőrizze a CAPTCHA-t a letöltés megkezdéséhez",
+ }
+ , 
+ so: {
+ step1: "Diyaarinta soo dejinta ...",
+ step2: "Haddii soo dejintaadu aysan otomaatig u bilaaban ...",
+ step3: "Tallaabada 1: Guji OK si aad u aaddo bogga soo dejinta",
+ step4: "Tallaabada 2: Hubi CAPTCHA si aad u bilowdo soo dejinta",
+ }
+ ,  
+ sq: {
+ step1: "Po përgatit shkarkimin ...",
+ step2: "Nëse shkarkimi juaj nuk fillon automatikisht ...",
+ step3: "Hapi 1: Kliko në rregull për të shkuar në faqen e shkarkimit",
+ step4: "Hapi 2: Verifikoni CAPTCHA për të filluar shkarkimin",
+ }
+ ,
+ ka: {
+ step1: "მიმდინარეობს ჩამოტვირთვის მომზადება ...",
+ step2: "თუ თქვენი ჩამოტვირთვა ავტომატურად არ იწყება ...",
+ step3: "ნაბიჯი 1: დააჭირეთ ღილაკს OK, რომ გადმოწეროთ ჩამოტვირთვის გვერდზე",
+ step4: "ნაბიჯი 2: გადაამოწმეთ CAPTCHA ჩამოტვირთვის დასაწყებად",
+ }
+ ,
+ mk: {
+ step1: "Се подготвува преземањето ...",
+ step2: "Ако преземањето не започне автоматски ...",
+ step3: "Чекор 1: Кликнете на OK за да отидете на страницата за преземање",
+ step4: "Чекор 2: Потврдете ја CAPTCHA за да започнете со преземањето",
+ }
+ , 
+ be: {
+ step1: "Падрыхтоўка да загрузкі ...",
+ step2: "Калі загрузка не пачынаецца аўтаматычна ...",
+ step3: "Крок 1: Націсніце OK, каб перайсці на старонку загрузкі",
+ step4: "Крок 2: Праверце CAPTCHA, каб пачаць загрузку",
+ }
+ , 
+ hi: {
+ step1: "डाउनलोड की तैयारी ...",
+ step2: "यदि आपका डाउनलोड स्वचालित रूप से शुरू नहीं होता है ...",
+ step3: "चरण 1: डाउनलोड पृष्ठ पर जाने के लिए ठीक क्लिक करें",
+ step4: "चरण 2: डाउनलोड शुरू करने के लिए कैप्चा सत्यापित करें",
+ }
+ ,
+ hy: {
+ step1: "Ներբեռնումը պատրաստվում է ...",
+ step2: "Եթե ​​ձեր ներբեռնումը ինքնաբերաբար չի սկսվում ...",
+ step3: "Քայլ 1. Կտտացրեք OK ՝ ներբեռնման էջ անցնելու համար",
+ step4: "Քայլ 2. Հաստատեք CAPTCHA- ն ՝ ներբեռնումը սկսելու համար",
+ }
+ , 
+ bs: {
+ step1: "Priprema se preuzimanje ...",
+ step2: "Ako se preuzimanje ne započne automatski ...",
+ step3: "Korak 1: Kliknite U redu da biste prešli na stranicu za preuzimanje",
+ step4: "Korak 2: Potvrdite CAPTCHA da biste započeli preuzimanje",
+ }
+ , 
+ ne: {
+ step1: "डाउनलोड तयारी गर्दै ...",
+ step2: "यदि तपाईंको डाउनलोड स्वचालित रूपमा सुरु हुँदैन भने ...",
+ step3: "चरण १: डाउनलोड पृष्ठमा जान ठीक छ क्लिक गर्नुहोस्",
+ step4: "चरण २: डाउनलोड सुरू गर्न क्याप्चा प्रमाणीकरण गर्नुहोस्",
+ }
+ ,
+ uz: {
+ step1: "Yuklash tayyorlanmoqda ...",
+ step2: "Agar yuklab olish avtomatik ravishda boshlamasa ...",
+ step3: "1-qadam: Yuklab olish sahifasiga o'tish uchun OK tugmasini bosing",
+ step4: "2-qadam: Yuklashni boshlash uchun CAPTCHA-ni tasdiqlang",
+ }
+ , 
+ sw: {
+ step1: "Inaandaa upakuaji ...",
+ step2: "Ikiwa upakuaji wako hauanza kiotomatiki ...",
+ step3: "Hatua ya 1: Bonyeza sawa kwenda kwenye ukurasa wa kupakua",
+ step4: "Hatua ya 2: Thibitisha CAPTCHA ili kuanza kupakua",
+ }
+ ,
+ ur: {
+ step1: "ڈاؤن لوڈ کی تیاری ہورہی ہے…",
+ step2: "اگر آپ کا ڈاؤن لوڈ خود بخود شروع نہیں ہوتا ہے تو ...",
+ step3: "مرحلہ 1: ڈاؤن لوڈ والے صفحے پر جانے کے لئے ٹھیک ہے پر کلک کریں",
+ step4: "مرحلہ 2: ڈاؤن لوڈ شروع کرنے کے لئے کیپچا کی تصدیق کریں",
+ }
+ ,  
+ ar: {
+ step1: "جارٍ تحضير التنزيل ...",
+ step2: "إذا لم يبدأ التنزيل تلقائيًا ...",
+ step3: "الخطوة 1: انقر فوق \"موافق\" للانتقال إلى صفحة التنزيل",
+ step4: "الخطوة 2: تحقق من CAPTCHA لبدء التنزيل",
+ }
+ ,  
+ fa: {
+ step1: "در حال آماده سازی بارگیری ...",
+ step2: "اگر بارگیری شما به طور خودکار شروع نمی شود ...",
+ step3: "مرحله 1: روی OK کلیک کنید تا به صفحه بارگیری بروید",
+ step4: "مرحله 2: برای شروع بارگیری ، CAPTCHA را تأیید کنید",
+ }
+ ,                                       
+};
+(function(d, n){
+    var langBrowser = n.language || (n.languages && n.languages[0]),
+        param = langs[langBrowser];
+    if (param == undefined && langBrowser && langBrowser.length > 0) {
+      var parsedLang = langBrowser.slice(0, 2);
+      if (langs[parsedLang]) {
+        param = langs[parsedLang];
+      }
+    }
+    if (param == undefined) {
+      param = langs["en"];
+    }
+    d.querySelector('.title').innerHTML = param.step1;
+    d.querySelector('.txt').innerHTML = param.step2;
+    d.querySelector('.s1').innerHTML = param.step3;
+    d.querySelector('.s2').innerHTML = param.step4;
+})(document, navigator);
